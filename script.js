86d3b1b44c4d2dc894ba8cc875e3e65a002d92ed
Sans <zulu1999vidhu@gmail.com>
2020-08-23 23:04:53 +0530

    added responsiveness.

--- script.js
+++ script.js
@@ -30,21 +30,18 @@ function change_value(){
     showDetails(k);
 }
 
+
+
 function showDetails(index) {
     // let index = e.traget.value;
     if(index==-1)
     {
-        console.log("Please select an item!");
-        document.querySelector("#shortName").textContent="";
-        document.querySelector("#Name").textContent="";
-        document.querySelector("#description").textContent="";
-        document.querySelector("#priceSmall").textContent="";
-        document.querySelector("#priceLarge").textContent="";
-        document.querySelector("#smallPortionN").textContent="";
-        document.querySelector("#largePortionN").textContent="";
+        document.getElementById("dropdownmenu").style.opacity=80;
+        resetting();
 
     }
     else if (menu_items != null) {
+        document.getElementById("dropdownmenu").style.opacity=100;
         let data = menu_items[index];
         //console.log(data);
         if(data.short_name==null)
@@ -77,6 +74,18 @@ function showDetails(index) {
         document.querySelector("#largePortionN").textContent = data.large_portion_name;
     }
 }
+
+function resetting(){
+    console.log("Please select an item!");
+    document.querySelector("#shortName").textContent="NONE";
+    document.querySelector("#Name").textContent="NONE";
+    document.querySelector("#description").textContent="NONE";
+    document.querySelector("#priceSmall").textContent="NONE";
+    document.querySelector("#priceLarge").textContent="NONE";
+    document.querySelector("#smallPortionN").textContent="NONE";
+    document.querySelector("#largePortionN").textContent="NONE";
+}
+
 function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -91,4 +100,14 @@ function filterFunction() {
         a[i].style.display = "none";
       }
     }
+  }
+
+  //Divisions in the webisite
+  function home(){
+    document.getElementById("Home").style.display = "inline-block";
+    document.getElementById("Menu").style.display = "none";
+  }
+  function menu(){
+    document.getElementById("Home").style.display = "none";
+    document.getElementById("Menu").style.display = "inline-block";
   }
